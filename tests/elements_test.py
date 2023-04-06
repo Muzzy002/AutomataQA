341ccd7a2349c1a8271fa80d5e611a6f41468a55
Muzzy002 <loginsilver21@gmail.com>
2023-04-06 16:36:51 +0300

    06.04.2023

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -6,7 +6,21 @@ class TestElements:
 	class TestTextBox:
 
 		def test_text_box(self, driver):
-			test_text_box = TextBoxPage(driver, "https://demoqa.com/text-box")
-			test_text_box.open()
-			test_text_box.fill_all_fields()
-			time.sleep(5)
+			test_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+			test_box_page.open()
+			full_name, email, current_address, permanent_address = test_box_page.fill_all_fields()
+			output_full_name, output_email, output_current_address, output_permanent_address = test_box_page.check_filled_form()
+
+
+			for i in full_name, email, current_address, permanent_address:
+				print('\n' + i)
+
+
+			assert full_name == output_full_name, "NAME FAILED"
+			assert email == output_email, "EMAIL FAILED"
+			assert current_address == output_current_address, "CUR ADR FAILED"
+			assert permanent_address == output_permanent_address, "PER ADR FAILED"
+
+
+
+
