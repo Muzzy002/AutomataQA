59a7420248122ca0ccb7f40998ef110f9d113670
Muzzy002 <loginsilver21@gmail.com>
2023-04-07 16:53:38 +0300

    06.04.2023

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -1,15 +1,26 @@
 from  selenium.webdriver.common.by import By
 
 class TextBoxPageLocators:
-    # form fields
+    # form fields # Поля для регистрации
     FULL_NAME = (By.CSS_SELECTOR, "input[id='userName']")
     EMAIL = (By.CSS_SELECTOR, 'input[id="userEmail"]')
     CURRENT_ADDRESS = (By.CSS_SELECTOR, 'textarea[id="currentAddress"]')
     PERMANENT_ADDRESS = (By.CSS_SELECTOR, 'textarea[id="permanentAddress"]')
     SUBMIT = (By.CSS_SELECTOR, 'button[id="submit"]')
 
-    # created from
+    # created from Поля для проверки после регистариции
     CREATED_FULL_NAME = (By.CSS_SELECTOR, '#output #name')
     CREATED_EMAIL = (By.CSS_SELECTOR, '#output #email')
     CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR, '#output #currentAddress')
     CREATED_PERMANENT_ADDRESS = (By.CSS_SELECTOR, '#output #permanentAddress')
+
+
+
+
+
+class CheckBoxPageLocators:
+
+    EXPAND_ALL_BUTTON = (By.CSS_SELECTOR, 'button[title="Expand all"]')
+    ITEM_LIST = (By.CSS_SELECTOR, 'span[class="rct-title"]')
+
+    CHECKED_ITEMS = (By.CSS_SELECTOR, 'svg[class="rct-icon rct-icon-half-check"]')
